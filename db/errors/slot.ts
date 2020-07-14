7ee188da8560304bdc167ea939a33c738bf2b505
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2020-07-14 01:48:35 -0300

    add error of day_required

--- db/errors/slot.ts
+++ db/errors/slot.ts
@@ -4,5 +4,5 @@ module.exports = {
   WEEKDAY_REQUIRED: 'You must inform the weekday, i.e: FRIDAY',
   MONTH_REQUIRED: 'you must inform the month on JS Date format!',
   HOUR_REQUIRED: 'Hour from Slot is required!',
-  DAY_REQUIRED: 'you must inform tHe day on JS Date format!',
+  DAY_REQUIRED: 'you must inform the month-day!',
 };
