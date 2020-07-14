e3d4ef60559d0e8f35f0e971b2925a507a9f7ab9
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2020-07-14 01:50:26 -0300

    implement agenda create and agenda book session

--- handlers/agenda/book.ts
+++ handlers/agenda/book.ts
@@ -1,42 +1,78 @@
 'use strict'
 
 import { connectToDatabase } from '../../db';
+import { getAroundSlotTime } from '../utils/getAroundSlotTime'
 import { Slot } from '../../db/models';
 
-module.exports.update = (event, context, callback) => {
+export const book = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
 
-    // retrieve professionalId
-    const { professionalId: string } = event.pathParameters.professionalId;
-    // retrieve patient booker name
-    // prepare identifier - hour+minute + '-' + professionalId
+    const { timeId, professionalId } :
+      { timeId: string, professionalId: string} = event.pathParameters;
+    const { bookerName } : { bookerName: string } = JSON.parse(event.body);
 
-    // get identifier
-    const { slotId: string } = event.pathParameters.id;
-    // verify if exists
-
-    // verify previous and next availability
-
-    // set booked to true and prev/next available to false
-    // set booker name
+    const times = timeId.split('-')
+    const [h, m, hour=Number(h), minute=Number(m)] = times.slice(-2);
+    const {nextHour, nextMinute, previousHour, previousMinute} = getAroundSlotTime(hour, minute);
+    const baseTimeId = times.slice(0,3).join('-');
 
     connectToDatabase()
-        .then(() => {
-            Slot.findById()
-            Slot.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body),
-                { new: true, runValidators: true })
-                .then(fracture => callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(fracture),
-                    headers: {
-                        'Access-Control-Allow-Origin': '*',
-                        'Access-Control-Allow-Credentials': true,
-                    }
-                }))
-                .catch(err => callback(null, {
-                    statusCode: err.statusCode || 500,
-                    headers: { 'Content-Type': 'text/plain' },
-                    body: JSON.stringify({msg:'Could not fetch the fracture for update.', err})
-                }));
-        });
+        .then(async () => {
+            const [previousSlot, slot, nextSlot] = await Slot.find(
+              {identifier: {
+                $in: [
+                  `${baseTimeId}-${previousHour}-${previousMinute}#${professionalId}`,
+                  `${timeId}#${professionalId}`,
+                  `${baseTimeId}-${nextHour}-${nextMinute}#${professionalId}`
+                ]}
+              })
+              .sort({timeSorter: 1});
+            let updatedSlot;
+            console.log(previousSlot, nextSlot)
+            if (!slot) {
+              throw JSON.stringify({
+                msg:`Couldn\'t book the slot because the informed timeId or professionalId doesn't exist.`,
+                values: {timeId, professionalId}})
+            } else if (slot.previousAvailable && slot.nextAvailable && !slot.booked) {
+              if (!nextSlot) {
+                throw JSON.stringify({
+                  msg:`Couldn\'t book the slot because the desired time and the last slot time are the same, since the sessions are 1 hour long you need to book at least 30 minutes before the last session`,
+                  values: {desiredTime: `${hour}:${minute}`, lastTime: `${slot.hour}:${slot.minutes}`}
+                })
+              }
+              if (nextSlot.booked || previousSlot.booked) {
+                throw JSON.stringify({
+                  msg: `Couldnt book the slot because the this slot belongs to a booked session`,
+                  values: {
+                    bookedConflict: nextSlot.booked ?
+                      `${nextSlot.hour}:${nextSlot.minutes}` :
+                      `${previousSlot.hour}:${previousSlot.minutes}`
+                  }
+                })
+              }
+
+              const updateBody = { previousAvailable: false, nextAvailable: false, booked: true, bookerName, professionalId };
+              updatedSlot = await Slot.findByIdAndUpdate(
+                slot._id, updateBody,
+                { new: true, runValidators: true });
+              return callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(updatedSlot),
+                headers: {
+                  'Access-Control-Allow-Origin': '*',
+                  'Access-Control-Allow-Credentials': true,
+                }
+              })
+            } else {
+              // err not available
+              throw JSON.stringify({msg:`Couldn\'t book the slot because it was booked already.`, values: {bookedFor: slot.bookerName}})
+            }
+        })
+      .catch(err => {
+        callback(null, {
+          statusCode: 400,
+          headers: { 'Content-Type': 'text/plain' },
+          body: err
+        })
+      });
 };
--- handlers/agenda/create.ts
+++ handlers/agenda/create.ts
@@ -4,24 +4,25 @@ import { splitAvailability } from '../utils/splitAvailability'
 import { Agenda } from '../../interfaces/agenda';
 const { Slot } = require('../../db/models');
 
-module.exports.create = (event, context, callback) => {
+export const create = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
     const promises = [];
 
     // todo: extract professional id from params
-    const { professionalId } = event.params;
-    const { agenda } : {agenda: [Agenda]} = event.body;
+    const { professionalId } = event.pathParameters;
+    const { agenda } : {agenda: [Agenda]} = JSON.parse(event.body);
 
     for (const day of agenda) {
         const slotToCreate = splitAvailability(day.availability)
         for (const slot of slotToCreate) {
             const slotBody = {
                 weekday: day.weekday,
+                day: day.day,
                 month: day.month,
                 year: day.year,
                 professionalId,
                 hour: slot.hour,
-                minute: slot.minute
+                minutes: slot.minute || '00',
             };
 
             promises.push(Slot.create(slotBody))
@@ -45,5 +46,9 @@ module.exports.create = (event, context, callback) => {
                     headers: { 'Content-Type': 'text/plain' },
                     body: JSON.stringify({msg:'Could not create the Slots for the agenda.', err})
                 }));
-        });
+        }).catch(err => callback(null, {
+            statusCode: err.statusCode || 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: JSON.stringify({msg:'Unexpected database error on creating the Slots for the agenda.', err})
+        }));
 };
